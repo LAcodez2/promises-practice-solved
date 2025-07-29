a09b4fbf7631494dede36a5b3a2572513b991470
Albert Valderas <albertvalderas052101@gmail.com>
2025-07-28 21:12:10 -0500

    e5-e8 solved

--- exercises/e5.js
+++ exercises/e5.js
@@ -8,8 +8,9 @@
  *          export const Promise
  */
 
-export const attachTitle = () => {
+export const attachTitle = (arg) => {
   // Your code goes here...
+  return `DR. ${arg}`;
 };
 
 /**
@@ -22,6 +23,12 @@ export const attachTitle = () => {
 
 export const getPromise = () => {
   // Your code goes here...
+  return Promise.resolve("MANHATTAN")
+    .then(attachTitle)
+    .then((result) => {
+      console.log(result);
+      return result;
+    });
 };
 
 // === TEST YOURSELF ===
